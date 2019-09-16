4b130682a554fb38374aa35eb4ab06fa0fedd19a
dimstav23 <dimstav23@gmail.com>
2019-09-17 00:28:28 +0200

    Navbar fixes (still remaining some) with insertion of jQuery

---
+++ js/general.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){ // here so that I know that all the items have been initialised
+
+  // Get the navbar
+  var navbar = document.getElementById("navbar");
+  var content = document.getElementById("pageContent");
+
+  // Get the offset position of the navbar
+  var sticky = navbar.offsetTop;
+  var navbarHeight = navbar.offsetHeight;
+  var moveOrNot = true;
+
+  //for the active highlight
+  let mainNavLinks = document.querySelectorAll(".topnav a");
+  let mainSections = document.querySelectorAll(".aboutme .education .skills .professionalexperience");
+  let lastId;
+  let cur = [];
+
+
+  $(window).on('scroll', function() {
+    let fromTop = window.scrollY;
+    /**
+      active selection
+    **/
+    mainNavLinks.forEach(link => {
+      let section = document.querySelector(link.hash);
+      if (
+        section.offsetTop <= fromTop + navbarHeight  &&
+        section.offsetTop + section.offsetHeight >= fromTop + navbarHeight
+      ) {
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
+      }
+    });
+    /**
+      sticky navbar
+    **/
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky");
+      content.classList.add("moveContentDown");
+    } else {
+      navbar.classList.remove("sticky");
+      content.classList.remove("moveContentDown");
+    }
+  });
+
+  $(mainNavLinks).click(function() {
+    //if we try to move to the same hash with the navbar, avoid this action
+    return !(this.hash === window.location.hash); // to remove the predefined handling
+  });
+
+  $(window).on('hashchange', function() {
+    if ( "onhashchange" in window ) {
+      var hash = window.location.hash.substring( 1 );
+      if ( !hash )
+          return;
+      var offset = navbarHeight;
+      var sel = '[id="' + hash + '"], a[name="' + hash + '"]';
+      var currentOffset = $( sel ).offset().top;
+      $( window ).scrollTop( currentOffset - offset );
+    }
+  });
+
+});
