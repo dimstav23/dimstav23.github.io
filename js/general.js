5b038e871f4ac1509630ca729f2ad75d8d070d77
dimstav23 <dimstav23@gmail.com>
2019-09-29 23:44:05 +0200

    Resizing issues / navbar adapted in smaller screens

--- js/general.js
+++ js/general.js
@@ -8,19 +8,20 @@ $(document).ready(function(){ // here so that I know that all the items have bee
   var sticky = navbar.offsetTop;
   var navbarHeight = navbar.offsetHeight;
   var moveOrNot = true;
-
+  var activeCounter = 1;
   //for the active highlight
-  let mainNavLinks = document.querySelectorAll(".topnav a");
+  let mainNavLinks = document.querySelectorAll(".topnav .link");
   let mainSections = document.querySelectorAll(".aboutme .education .skills .professionalexperience");
   let lastId;
   let cur = [];
-
 
   $(window).on('scroll', function() {
     let fromTop = window.scrollY;
     /**
       active selection
     **/
+    //console.log("offset Top : " + navbar.offsetTop + " offset height: " + navbar.offsetHeight + " page offset " + window.pageYOffset)
+    //navbarHeight = navbar.offsetHeight;
     mainNavLinks.forEach(link => {
       let section = document.querySelector(link.hash);
       if (
@@ -63,10 +64,30 @@ $(document).ready(function(){ // here so that I know that all the items have bee
     var hash = window.location.hash.substring( 1 );
     if ( !hash )
         return;
+
+    // vanish the dropdown menu so that the page is visible and in order to calculate
+    // offset correctly
+    var x = document.getElementById("navbar");
+    if (x.className === "topnav responsive" || x.className === "topnav sticky responsive"){
+          x.classList.remove('responsive');
+    }
+
     var offset = navbarHeight;
     var sel = '[id="' + hash + '"], a[name="' + hash + '"]';
     var currentOffset = $( sel ).offset().top;
+    console.log(currentOffset + " " + offset);
     $( window ).scrollTop( currentOffset - offset );
   }
+
+  //on click handler for the responsive navbar
+  document.querySelector('.icon').addEventListener('click', function() {
+    var x = document.getElementById("navbar");
+    if (x.className === "topnav" || x.className === "topnav sticky") {
+      x.classList.add('responsive');
+    } else {
+      x.classList.remove('responsive');
+    }
+    return true;
+  })
 
 });
